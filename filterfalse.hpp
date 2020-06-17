36e5b8ce8799c613c1c48ebfa4cc35e22781529a
OrenZauda <45006271+OrenZauda@users.noreply.github.com>
2020-06-17 14:57:47 +0300

    Add files via upload

--- filterfalse.hpp
+++ filterfalse.hpp
@@ -1,27 +1,26 @@
 #pragma once
-#include <boost/function.hpp>
+//#include <boost/function.hpp>
 #include <vector>
 namespace itertools{
 
-        template <typename T> 
+        template <typename T,typename T1> 
 
     class filterfalse{
+        
         T container;
         using v1= decltype(container.begin());
         using v2= decltype(*(container.begin()));
-        using v3 = boost::function<bool (int x)> ;
-
-        v3 fun2;
+        T1 fun2;
 
         class iterator{
             public:
             v1 ptr;
             v1 endd;
-            v3 f2;
+            T1 f2;
             T c;
 
            
-            iterator(v1 it, v3 a,T cn,v1 e):ptr(it),f2(a),c(cn),endd(e){
+            iterator(v1 it, T1 a,T cn,v1 e):ptr(it),f2(a),c(cn),endd(e){
 
             }
             iterator(v1 it,T cc):ptr(it),c(cc){
@@ -52,7 +51,7 @@ namespace itertools{
         public:
        
 
-        filterfalse(v3 strct,T cont):container(cont),fun2(strct){
+        filterfalse(T1 strct,T cont):container(cont),fun2(strct){
         };
         
         
